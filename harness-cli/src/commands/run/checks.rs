fa3a0aae68347f1f9bfefbb0e241940d74ba0e03
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2024-03-07 11:36:57 +1100

    Fix build error

--- harness-cli/src/commands/run/checks.rs
+++ harness-cli/src/commands/run/checks.rs
@@ -7,14 +7,17 @@ struct PreBenchmarkingChecker {
     allow_dirty: bool,
     #[allow(unused)]
     allow_multi_user: bool,
+    #[allow(unused)]
+    allow_any_scaling_governor: bool,
 }
 
 impl PreBenchmarkingChecker {
-    fn new(allow_dirty: bool, allow_multi_user: bool) -> Self {
+    fn new(allow_dirty: bool, allow_multi_user: bool, allow_any_scaling_governor: bool) -> Self {
         Self {
             warnings: Vec::new(),
             allow_dirty,
             allow_multi_user,
+            allow_any_scaling_governor,
         }
     }
 
@@ -22,7 +25,7 @@ impl PreBenchmarkingChecker {
         self.warnings.push(msg.as_ref().to_owned());
     }
 
-    fn dirty_git_worktree_check(&mut self) -> anyhow::Result<()> {
+    fn check_dirty_git_worktree(&mut self) -> anyhow::Result<()> {
         let git_info = git_info::get();
         let Some(dirty) = git_info.dirty else {
             anyhow::bail!("No git repo found");
@@ -39,7 +42,7 @@ impl PreBenchmarkingChecker {
     #[cfg(target_os = "linux")]
     fn check(&mut self) -> anyhow::Result<()> {
         // Check if the current git worktree is dirty
-        self.dirty_git_worktree_check()?;
+        self.check_dirty_git_worktree()?;
         // Check if the current user is the only one logged in
         if PLATFORM_INFO.users.len() > 1 {
             let msg = format!(
@@ -88,45 +91,12 @@ impl PreBenchmarkingChecker {
 }
 
 impl super::RunArgs {
-    #[cfg(target_os = "linux")]
     pub(super) fn pre_benchmarking_checks(&self) -> anyhow::Result<()> {
-        let mut errors = Vec::new();
-        // Check if the current git worktree is dirty
-        self.dirty_git_worktree_check()?;
-        // Check if the current user is the only one logged in
-        if PLATFORM_INFO.users.len() > 1 {
-            let msg = format!(
-                "More than one user logged in: {}",
-                PLATFORM_INFO.users.join(", ")
-            );
-            if self.allow_multi_user {
-                eprintln!("🚨 WARNING: {}", msg);
-            } else {
-                anyhow::bail!("{}", msg);
-            }
-        }
-        // Check if all the scaling governors are set to `performance`
-        if !PLATFORM_INFO
-            .scaling_governor
-            .iter()
-            .all(|g| g == "performance")
-        {
-            let msg = format!(
-                "Not all scaling governors are set to `performance`: [{}]",
-                PLATFORM_INFO.scaling_governor.join(", ")
-            );
-            if self.allow_any_scaling_governor {
-                eprintln!("🚨 WARNING: {}", msg);
-            } else {
-                anyhow::bail!("{}", msg);
-            }
-        }
-        Ok(())
-    }
-
-    #[cfg(not(target_os = "linux"))]
-    pub(super) fn pre_benchmarking_checks(&self) -> anyhow::Result<()> {
-        let mut checker = PreBenchmarkingChecker::new(self.allow_dirty, self.allow_multi_user);
+        let mut checker = PreBenchmarkingChecker::new(
+            self.allow_dirty,
+            self.allow_multi_user,
+            self.allow_any_scaling_governor,
+        );
         checker.check()?;
         checker.dump_warnings();
         Ok(())
