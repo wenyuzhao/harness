5f5c66c11551869746c4fb569e391ed0aa38aec7
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2024-03-08 10:28:04 +1100

    restore git state

--- harness-cli/src/lib.rs
+++ harness-cli/src/lib.rs
@@ -1,5 +1,6 @@
 use clap::{Parser, Subcommand};
 use colored::Colorize;
+use git_info::types::GitInfo;
 use once_cell::sync::Lazy;
 
 #[macro_use]
@@ -36,15 +37,37 @@ static CMD_ARGS: Lazy<Cli> = Lazy::new(|| {
     Cli::parse_from(args)
 });
 
+fn restore_git_state(prev: &GitInfo) {
+    let curr = git_info::get();
+    if prev.head.last_commit_hash != curr.head.last_commit_hash {
+        let mut command = std::process::Command::new("git");
+        command
+            .arg("checkout")
+            .arg(prev.head.last_commit_hash.as_ref().unwrap());
+        if let Ok(status) = command.status() {
+            if !status.success() {
+                eprintln!(
+                    "❌ {}: Failed to checkout to previous commit",
+                    "ERROR".red().bold()
+                );
+                std::process::exit(1);
+            }
+        }
+    }
+}
+
 #[doc(hidden)]
 pub fn main() -> anyhow::Result<()> {
+    let git = git_info::get();
     let result = match &CMD_ARGS.command {
         Commands::Run(cmd) => cmd.run(),
         Commands::Report(cmd) => cmd.run(),
     };
     if let Err(err) = result {
         eprintln!("❌ {}: {}", "ERROR".red().bold(), err.to_string().red());
+        restore_git_state(&git);
         std::process::exit(1);
     }
+    restore_git_state(&git);
     Ok(())
 }
