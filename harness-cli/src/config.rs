c95ff36ade36cf5b44ec6cd0973b1e7cb13547f9
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2024-03-08 21:33:20 +1100

    default harness config

--- harness-cli/src/config.rs
+++ harness-cli/src/config.rs
@@ -11,25 +11,39 @@ struct CargoConfig {
 
 #[derive(Deserialize)]
 struct CargoConfigPackage {
-    metadata: CargoConfigPackageMetadata,
+    metadata: Option<CargoConfigPackageMetadata>,
     #[serde(flatten)]
     _others: HashMap<String, toml::Value>,
 }
 
 #[derive(Deserialize)]
 struct CargoConfigPackageMetadata {
-    harness: Config,
+    harness: Option<HarnessConfig>,
     #[serde(flatten)]
     _others: HashMap<String, toml::Value>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Config {
+pub struct HarnessConfig {
     pub profiles: HashMap<String, Profile>,
 }
 
-fn one() -> usize {
-    1
+impl Default for HarnessConfig {
+    fn default() -> Self {
+        Self {
+            profiles: [("default".to_owned(), Default::default())]
+                .into_iter()
+                .collect(),
+        }
+    }
+}
+
+fn default_iterations() -> usize {
+    5
+}
+
+fn default_invocations() -> usize {
+    10
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -41,13 +55,26 @@ pub struct Profile {
     #[serde(default)]
     pub builds: HashMap<String, BuildConfig>,
     /// Number of iterations
-    #[serde(default = "one")]
+    #[serde(default = "default_iterations")]
     pub iterations: usize,
     /// Number of invocations
-    #[serde(default = "one")]
+    #[serde(default = "default_invocations")]
     pub invocations: usize,
     /// The baseline build name. This is only used for data reporting.
     pub baseline: Option<String>,
+}
+
+impl Default for Profile {
+    fn default() -> Self {
+        Self {
+            probes: Vec::new(),
+            env: HashMap::new(),
+            builds: HashMap::new(),
+            iterations: default_iterations(),
+            invocations: default_invocations(),
+            baseline: None,
+        }
+    }
 }
 
 fn default_true() -> bool {
@@ -77,10 +104,23 @@ impl Default for BuildConfig {
     }
 }
 
-pub fn load_from_cargo_toml() -> anyhow::Result<Config> {
+/// Load the harness configuration from the `Cargo.toml` file
+/// If the `harness` section is not present, a default config with a default profile is returned.
+pub fn load_from_cargo_toml() -> anyhow::Result<HarnessConfig> {
     if !PathBuf::from("./Cargo.toml").is_file() {
         anyhow::bail!("Failed to load ./Cargo.toml");
     }
     let s = std::fs::read_to_string("./Cargo.toml")?;
-    Ok(toml::from_str::<CargoConfig>(&s)?.package.metadata.harness)
+    let mut harness = toml::from_str::<CargoConfig>(&s)?
+        .package
+        .metadata
+        .map(|m| m.harness)
+        .flatten()
+        .unwrap_or_default();
+    if harness.profiles.is_empty() {
+        harness
+            .profiles
+            .insert("default".to_owned(), Default::default());
+    }
+    Ok(harness)
 }
